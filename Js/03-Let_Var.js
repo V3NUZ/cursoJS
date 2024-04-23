faa69afd21b9bf2afdf1d0d7ceb9c3bea2d6c00e
Alejandro Cardona <yeisoonc@gmail.com>
2024-04-23 16:44:55 -0500

    Let y Var Js

---
+++ Js/03-Let_Var.js
@@ -0,0 +1,23 @@
+//Prueba con VAR
+
+var numero = 40;
+console.log(numero);
+
+if (true){
+    var numero=50;
+    console.log(numero);
+}
+
+console.log(numero);
+
+//Prueba con LET
+
+var texto = "Cursos de Js";
+console.log(texto);
+
+if (true){
+    let texto = "Asi se aprende Js";
+    console.log(texto);
+}
+
+console.log(texto);
