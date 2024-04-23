48f69c3875e04c58a131f739cea455e38bbe6928
Alejandro Cardona <yeisoonc@gmail.com>
2024-04-23 16:35:35 -0500

    Variables en Js

---
+++ Js/02-Variables.js
@@ -0,0 +1,14 @@
+// Variables
+// Una variable es un contenedor de info.
+
+var pais = "Colombia";
+var continente = "LATAM";
+var antiguedad = "2022";
+var pais_y_continenete = pais + ' ' + continente;
+
+
+
+pais = "Mexico";
+
+console.log(pais,continente,antiguedad);
+alert(pais_y_continenete);
