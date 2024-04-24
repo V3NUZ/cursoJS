6f7f73d1b113a1adee245341274816c72032cc31
Alejandro Cardona <yeisoonc@gmail.com>
2024-04-24 08:22:11 -0500

    Tipos de Datos JS

---
+++ Js/05_Tipos_Datos.js
@@ -0,0 +1,19 @@
+//Operadores
+var numero1 = 7;
+var numero2 = 10;
+var operacion = numero1 * numero2;
+
+alert("El resultado de la suma es: " + operacion);
+
+//Tipos de datos
+//Entero, String, Flotante, Boolean, Array y Objetos
+
+var numero_entero = 44;
+var cadena_texto = "Hola";
+var verdadero_o_falso = true;
+
+var numero_falso = "40";
+
+console.log(String(numero_falso)+5);
+
+console.log(typeof numero_falso);
