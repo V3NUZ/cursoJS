325e524584dd1896e6dd1363eb05ca4ea3756ea8
Alejandro Cardona <yeisoonc@gmail.com>
2024-04-26 16:09:00 -0500

    Ventanas en Js

---
+++ Js/10-Ventajas.js
@@ -0,0 +1,10 @@
+
+/*alert("Esta es mi alerta");
+alert("Este es mi texto");*/
+
+var mi_resultado = confirm("Estas seguro que quiere seguir perro?");
+console.log(mi_resultado);
+
+//Ingreso datos
+var relsultado = prompt("¿Que edad tienes?", 18);
+console.log(relsultado);
