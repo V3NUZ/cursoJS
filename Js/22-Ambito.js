2f51360fb5fc67148b51d077439ef5d1860e3615
Alejandro <yeisoonc159@gmail.com>
2024-10-28 16:39:54 -0500

    Convertir dato a un String

---
+++ Js/22-Ambito.js
@@ -0,0 +1,11 @@
+
+var texto = "Hola mundo soy una variable global";
+var numero = 12;
+
+function holaMundo(texto){
+    console.log(texto);
+    console.log(numero.toString());
+}
+
+holaMundo(texto);
+
